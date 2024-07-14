a21ca253c565d4d6223c58de4d955a13ce1a48cc
Ananya Pathak <79246149+oldskoolmoron@users.noreply.github.com>
2024-07-14 15:40:13 +0000

    some more arrays and JSON call

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -27,12 +27,96 @@ JsUser.email = "abc@openAI.com"
 // console.log(JsUser)
 
 JsUser.greeting = function(){
-    console.log("Hello JS community")
+    // console.log("Hello JS community")
 }
 
 JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`)
+    // console.log(`Hello JS user, ${this.name}`)
 }
-console.log(JsUser.greeting())
-console.log(JsUser.greetingTwo())
+// console.log(JsUser.greeting())
+// console.log(JsUser.greetingTwo())
 
+const twitteruser = new Object()
+
+const twrtteruser = {}
+
+twitteruser.id = "123abc"
+twitteruser.name = "boody"
+twitteruser.isLoggedIn = false
+
+// console.log(twitteruser)
+
+const regularuser = {
+    email : "some@gmail.com",
+    fullName : {
+        userfullname : {
+            firstname : "moron",
+            lastName : "pathak"
+        }
+    }
+}
+//console.log(regularuser.fullName.userfullname)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj5 = {5: "e", 6: "f"}
+
+// const obj3 = {obj1 , obj2}
+
+// const obj3 = Object.assign({},obj1, obj2) // {} = target, and rest all are source = obj1,obj2,obj3
+// console.log(obj3)
+
+const obj3 = {...obj1, ...obj2, ...obj5}
+// console.log(obj3);
+
+const users = [
+    {
+        id : 1,
+        email : "boody@gmail.com"
+    },{
+        id : 2,
+        email : "abc@gmial.com"
+    },{
+        id : 3,
+        email : "xyz@gmail.com"
+    }
+]
+
+// console.log(users[0].email);
+
+// console.log(twitteruser);
+
+// console.log(Object.keys(twitteruser));
+// console.log(Object.values(twitteruser));
+// console.log(Object.entries(twitteruser));
+
+// console.log(twitteruser.hasOwnProperty('isLoggedIn'));
+
+                                            //de-structing in Objects
+
+const course = {
+    coursename : "js series",
+    price : "freefreefree",
+    courseInstructer : "Hitesh Sir"
+}
+
+// console.log(course.courseInstructer)
+
+const {courseInstructer : instructer} = course
+
+//console.log(instructer);
+
+                                            // API concept
+// https://api.github.com/users/oldskoolmoron
+
+// { api call using object(JSON)
+//     "name" : "h",
+//     "coursename" : "js in hindi",
+//     "price" : "free"
+// }
+
+// [ api call using array (JSON)
+//     {},
+//     {},
+//     {}
+// ]
