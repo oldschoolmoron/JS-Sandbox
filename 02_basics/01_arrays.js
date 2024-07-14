4a2059bbb823bf400707da1feb04b5faad93741b
Ananya Pathak <79246149+oldskoolmoron@users.noreply.github.com>
2024-07-14 10:26:49 +0000

    Completed moderate review of arrays in JavaScript

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -22,16 +22,45 @@ const NewArr = ArrofHeros.join()
 
 //slice and splice
 
-console.log("A ", Arr);
+// console.log("A ", Arr);
 
 const myarr1 = Arr.slice(1 , 4);
-console.log(myarr1);
-console.log("B", Arr)
+// console.log(myarr1);
+// console.log("B", Arr)
 
 const myarr2 = Arr.splice(1, 4);
-console.log(myarr2);
-console.log("C", Arr)
+// console.log(myarr2);
+// console.log("C", Arr)
 // 'slice' returns a shallow copy of a portion of an array without modifying the original array, while 'splice' modifies the original array by removing or adding elements.
 
+// Array part - 2
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman","flash","batman"]
+
+// marvel_heros.push(dc_heros) o/p = [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// marvel_heros.concat(dc_heros)// output = [ 'thor', 'Ironman', 'spiderman' ]
+const Allheros = marvel_heros.concat(dc_heros)
+// console.log(Allheros)
+
+const All_new_heros = [...marvel_heros, ...dc_heros];
+// console.log(All_new_heros)
+
+const anoth_arr = [1, 2 ,3, [4, 5, 6],7 , [8, 9, [10, 11]]]
+
+const original_form = anoth_arr.flat(Infinity)
+// console.log(original_form);
+
+console.log(Array.isArray("moron"))
+console.log(Array.from("moron"))
+console.log(Array.from({name : "moron"}));
+
+let num1 = 10
+let num2 = 20
+let num3 = 30
+
+console.log(Array.of(num1,num2,num3))
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
