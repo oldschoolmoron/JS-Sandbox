096ffb5dcf39264f76ed8a45a072f630a56c5519
Ananya Pathak <79246149+oldskoolmoron@users.noreply.github.com>
2024-07-18 14:44:06 +0000

    Everything about scope in js

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -7,11 +7,57 @@ if(true){
 let a = 10
 //const b = 20
 var c = 30
-console.log("INdex : ",a)
+// console.log("INdex : ",a)
 }
 
-console.log(a)
-//console.log(b)
-console.log(c)
+// console.log(a)
+// console.log(b)
+// console.log(c)
 
 // {} => scope
+
+function one(){
+    const username = "moron"
+    function two(){
+        const website = "youtube.com"
+        console.log(username)
+    }
+    // console.log(website);
+    //two()
+
+}
+//one()
+
+if(true){
+    const username = "xyz"
+    if(username === "xyz"){
+        const website = " youtube"
+        //console.log(username + website)
+    }
+    // console.log(website)
+}
+
+// console.log(username)
+
+
+                                        //part -3 (FUNCTION HOISTING)
+                                        
+addone(5)
+function addone(num){
+    return num + 1
+}
+
+//addtwo(5)
+const addtwo = function(num){
+    return num +2
+}
+
+
+
+
+
+
+
+
+
+
