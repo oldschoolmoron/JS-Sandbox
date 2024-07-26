42a1a40ae04a189101ff020aedfd32bb2191c20c
Ananya Pathak <79246149+oldskoolmoron@users.noreply.github.com>
2024-07-26 15:25:04 +0000

    all About switch case in js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch(key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "jan"
+
+switch (month){
+    case "jan":
+        console.log("January") // to create duplicate press shift + option + down arrow (for mac)
+        break;
+    case "feb":
+        console.log("february")
+        //break;
+    case "mar":
+        console.log("March")
+        //break;
+    case "ape":
+        console.log("april fool")
+        break;
+
+    default:
+        console.log("out of space thing")
+        break;
+}
