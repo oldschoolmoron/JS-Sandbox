1c332289bb64d136e72cdfd9f0c3ebcf96199351
Ananya Pathak <79246149+oldskoolmoron@users.noreply.github.com>
2024-07-29 20:35:38 +0000

    everything about for loop

---
+++ 05_iterators/one.js
@@ -0,0 +1,33 @@
+// for loop 
+
+for (let i = 0; i < 10; i++) { //trl + D is used to select the next occurrence of the current 
+                                // selection, which is useful for making multiple changes at once.
+
+    const element = i; 
+    if(element == 5){
+    console.log("5 is the best element")}
+    
+}
+
+for(let i=0; i<2; i++){
+    // console.log(`outer loop val : ${i}`);
+    for(let j=1; j <= 10; j++){
+        // console.log(`Inner loop val ${j} and inner loop ${i}`);
+        // console.log(i + ' * ' + j + ' = ' + i*j)
+    }
+}
+let myArray = ["flash", "postman", "shaktiman"]
+// console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+}
+
+//break and continue
+
+for (let index = 0; index < 5; index++) {
+    // if(index == 2) break;
+    if(index == 2) continue;
+    console.log(`val of i is ${index}`);
+}
+
