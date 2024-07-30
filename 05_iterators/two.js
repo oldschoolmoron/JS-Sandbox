697eb7aa5b22c521d8f2e41dd42b611b9267c4cb
Ananya Pathak <79246149+oldskoolmoron@users.noreply.github.com>
2024-07-30 19:31:18 +0000

    basics of while n do while loop

---
+++ 05_iterators/two.js
@@ -0,0 +1,23 @@
+// while
+
+// while(condition){
+//     statement;
+// }
+let index =0;
+while(index<=10){
+    // console.log(`val of index is ${index}`)
+    index = index + 3;
+}
+let myArray = ['ironman', 'batman','captain america']
+let arr = 0;
+
+while(arr < myArray.length){
+    // console.log(`val is ${myArray[arr]}`);
+    arr++
+}
+let max =1
+do{
+    // console.log(`score is ${max}`);
+    max++;
+}while(max <= 10)
+
